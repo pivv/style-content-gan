8feb4ab1dbdbe310e303ffb38b4242c055e6e392
andyjeon <andyjeon@snu.ac.kr>
2021-04-06 19:26:39 +0900

    add debuging afhq_style_dog_loader.py

--- csgan/loader/afhq_style_dog_loader.py
+++ csgan/loader/afhq_style_dog_loader.py
@@ -39,14 +39,19 @@ class StylingDogDataset(Dataset):
         style_dog_name: str = np.random.choice(self._style_dog_names)
         white_dog_image: np.ndarray = cv2.imread(os.path.join(self._white_dog_dir, white_dog_name))
         style_dog_image: np.ndarray = cv2.imread(os.path.join(self._style_dog_dir, style_dog_name))
-
-        white_dog_image = cv2.resize(white_dog_image, (self._image_size, self._image_size),
+        
+        try:    
+            white_dog_image = cv2.resize(white_dog_image, (self._image_size, self._image_size),
+                                          interpolation=cv2.INTER_CUBIC)
+            style_dog_image = cv2.resize(style_dog_image, (self._image_size, self._image_size),
                                       interpolation=cv2.INTER_CUBIC)
-        style_dog_image = cv2.resize(style_dog_image, (self._image_size, self._image_size),
-                                  interpolation=cv2.INTER_CUBIC)
+        except: 
+            print(f"[DEBUG] {os.path.join(self._white_dog_dir, white_dog_name)}")
+            print(f"[DEBUG] {os.path.join(self._style_dog_dir, style_dog_name)}")
 
         white_dog_image = cv2.cvtColor(white_dog_image, cv2.COLOR_BGR2RGB).astype(np.float32) / 255.
         style_dog_image = cv2.cvtColor(style_dog_image, cv2.COLOR_BGR2RGB).astype(np.float32) / 255.
+
 
         x1 = torch.FloatTensor(white_dog_image.transpose((2, 0, 1)))
         x2 = torch.FloatTensor(style_dog_image.transpose((2, 0, 1)))
