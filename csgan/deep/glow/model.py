6e543c5eafbe0c272857dd67e06a7f1009dca2d1
pivv <pivv@snu.ac.kr>
2021-04-16 18:06:38 +0900

    Glow start

--- csgan/deep/glow/model.py
+++ csgan/deep/glow/model.py
@@ -413,7 +413,8 @@ def calc_z_shapes(n_channel, input_size, n_flow, n_block):
         z_shapes.append((n_channel, input_size, input_size))
 
     input_size //= 2
-    z_shapes.append((n_channel * 4, input_size, input_size))
+    n_channel *= 4
+    z_shapes.append((n_channel, input_size, input_size))
 
     return z_shapes
 
